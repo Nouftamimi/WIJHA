aea1880a8b53102783ade3e86d3e7682ac14a796
Nouf Altamimi <nouftamimi1@gmail.com>
2022-12-07 15:41:26 +0300

    Initial Commit

---
+++ Bookmarked/BookmarkedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookmarkedApp.swift
+//  Bookmarked
+//
+//  Created by Nouf  on 13/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct BookmarkedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
